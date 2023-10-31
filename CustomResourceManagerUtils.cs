6d199084e36ce3c218eb7f3e3ee6b94025ecde11
Emeersonts <111031202+Emeersonts@users.noreply.github.com>
2023-10-31 19:03:32 -0300

    Add files via upload

---
+++ CustomResourceManagerUtils.cs
@@ -0,0 +1,12 @@
+﻿using System.Resources;
+
+namespace BackOffice.Authorizer.Management.I18N
+{
+    public static class CustomResourceManagerUtils
+    {
+        public static ResourceManager GetResourceManager()
+        {
+            return new ResourceManager("BackOffice.Authorizer.Management.I18N.Messages", typeof(CustomResourceManagerUtils).Assembly);
+        }
+    }
+}
